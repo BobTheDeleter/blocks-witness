8eb8bbfee2c8ca235ecbcea5f413dd46042333ca
BobTheDeleter <anishkoduri@gmail.com>
2025-09-18 19:20:47 +0100

    restructure level ordering to be more flexiible
    remove level 21

--- autoloads/progression.gd
+++ autoloads/progression.gd
@@ -6,9 +6,11 @@ var current_level: int = -1:
 		highest_completed_level = max(value - 1, highest_completed_level)
 
 func _on_change_level(level_num: int) -> void:
-	if not does_level_exist(level_num):
+	if level_num + 1 > levels.size():
 		return
-	get_tree().change_scene_to_file("res://level_data/" + str(level_num) + ".tscn")
+	get_tree().change_scene_to_packed(levels[level_num])
 
 func does_level_exist(level_num: int) -> bool:
 	return ResourceLoader.exists("res://level_data/" + str(level_num) + ".tscn")
+
+@onready var levels: Array[PackedScene] = preload("res://level_data/levels.tres").levels
---
+++ level/level_scenes.gd
@@ -0,0 +1,4 @@
+class_name LevelScenes
+extends Resource
+
+@export var levels: Array[PackedScene]
