bc9f32bf18c54111c8399427908d2c25fe77e7f1
BobTheDeleter <anishkoduri@gmail.com>
2025-09-18 11:45:05 +0100

    restructure piece interaction convert main menu to all controls

--- menu/play.gd
+++ menu/play.gd
@@ -1,14 +1,7 @@
-extends Sprite2D
+extends TextureButton
 
-const default_texture: Texture2D = preload("res://menu/Default.svg")
-const hover_texture: Texture2D = preload("res://menu/Hover.svg")
-
-func _on_area_2d_input_event(_viewport:Node, event:InputEvent, _shape_idx:int) -> void:
-    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-        Audio.play_sfx(Audio.SFX.CLICK)
-        get_tree().change_scene_to_file("res://level_data/0.tscn")
-
-func _on_area_2d_mouse_exited() -> void:
-    texture = default_texture
-func _on_area_2d_mouse_entered() -> void:
-    texture = hover_texture
+func _on_pressed() -> void:
+	if %CheckBox.button_pressed:
+		Progression.highest_completed_level = 1000
+	Audio.play_sfx(Audio.SFX.CLICK)
+	get_tree().change_scene_to_file("res://level_data/0.tscn")
--- piece/piece.gd
+++ piece/piece.gd
@@ -25,7 +25,6 @@ func _ready() -> void:
 	create_area2d()
 	area2d.mouse_entered.connect(_on_mouse_entered)
 	area2d.mouse_exited.connect(_on_mouse_exited)
-	area2d.input_event.connect(_on_input_event)
 
 	# const offset_range = 0.3
 	# var colour_offset = Color(randf_range(-offset_range, offset_range), randf_range(-offset_range, offset_range), randf_range(-offset_range, offset_range))
@@ -76,31 +75,14 @@ func _draw() -> void:
 	
 	draw_multiline(boundary, line_colour, line_thickness * 2)
 
+var mouse_over: bool = false
 func _on_mouse_entered() -> void:
 	# Optional: highlight or change appearance
 	modulate = Color(1, 1, 0.8)
-
+	mouse_over = true
 func _on_mouse_exited() -> void:
 	modulate = Color(1, 1, 1)
-
-func _on_input_event(_viewport, event, _shape_idx) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if state != State.PICKED_UP:
-			# Emit signal to notify that the piece is picked up
-			for piece in get_parent().board.pieces:
-				if piece.state == Piece.State.PICKED_UP:
-					piece.state = Piece.State.IN_PALETTE
-			emit_signal("piece_picked_up", self)
-			state = State.PICKED_UP
-			modulate = Color(1, 1, 0.5) # Change appearance when picked up
-		else: 
-			# Emit the signal to notify that the piece is placed
-			# Let either the board or the palette detect where it is
-			emit_signal("piece_placed", self)
-
-	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
-		if state != State.IN_BOARD:
-			rotate_piece()
+	mouse_over = false
 
 func rotate_piece() -> void:
 	# Rotate the piece initial_data blocks by 90 degrees clockwise
@@ -127,6 +109,24 @@ func _process(_delta):
 			global_position = Vector2(origin_position_grid * cell_size_px)
 		_:
 			pass
+
+	if Input.is_action_just_pressed("pickup_place") and mouse_over:
+		if state != State.PICKED_UP:
+			# Emit signal to notify that the piece is picked up
+			for piece in get_parent().board.pieces:
+				if piece.state == Piece.State.PICKED_UP:
+					piece.state = Piece.State.IN_PALETTE
+			emit_signal("piece_picked_up", self)
+			state = State.PICKED_UP
+			modulate = Color(1, 1, 0.5) # Change appearance when picked up
+		else: 
+			# Emit the signal to notify that the piece is placed
+			# Let either the board or the palette detect where it is
+			emit_signal("piece_placed", self)
+
+	if Input.is_action_just_pressed("rotate") and mouse_over:
+		if state != State.IN_BOARD:
+			rotate_piece()
 
 func move_out_of_board() -> void:
 	# if the piece rect intersects the board, move it along the piece's centre - board centre direction
