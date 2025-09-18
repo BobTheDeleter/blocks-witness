9eb7b2e28134153a6d6f6722616163f6617523ee
BobTheDeleter <anishkoduri@gmail.com>
2025-09-18 19:33:09 +0100

    made covered and not_covered crosses for more visual clarity

--- element/covered.gd
+++ element/covered.gd
@@ -1,19 +1,32 @@
 extends Element
 
 var line_thickness: float
-var radius: float
 var centre: Vector2
+var cell_size_px: float
 
 const type = TYPE.COVERED
 
 func initialise(board: Board) -> void:
 	colour = Colours.PINK
 	line_thickness = board.line_thickness
-	radius = (board.cell_size_px - line_thickness) / 2 - board.cell_size_px * 0.1
+	cell_size_px = board.cell_size_px
 	centre = Vector2(board.cell_size_px, board.cell_size_px) / 2
 
 func _draw() -> void:
-	draw_circle(centre, radius, colour)
+	# draw a verticle rectangle in PINK
+	draw_rect(Rect2(
+		Vector2(cell_size_px / 2 - cell_size_px / 8, cell_size_px / 8),
+		Vector2(cell_size_px / 4, cell_size_px * 3 / 4)
+	),
+		colour
+	)
+	# draw a horizontal rectangle in PINK
+	draw_rect(Rect2(
+		Vector2(cell_size_px / 8, cell_size_px / 2 - cell_size_px / 8),
+		Vector2(cell_size_px * 3 / 4, cell_size_px / 4)
+	),
+		colour
+	)
 
 func check(data) -> bool:
 	return data[position_grid]["piece"] != null
--- element/not_covered.gd
+++ element/not_covered.gd
@@ -1,19 +1,33 @@
 extends Element
 
 var line_thickness: float
-var radius: float
 var centre: Vector2
+var cell_size_px: float
 
 const type = TYPE.NOT_COVERED
 
 func initialise(board: Board) -> void:
 	colour = Colours.ORANGE
 	line_thickness = board.line_thickness
-	radius = (board.cell_size_px - line_thickness) / 2 - board.cell_size_px * 0.1
+	cell_size_px = board.cell_size_px
 	centre = Vector2(board.cell_size_px, board.cell_size_px) / 2
 
 func _draw() -> void:
-	draw_circle(centre, radius, colour)
+	draw_set_transform(Vector2(cell_size_px / 2, ((0.5)-(sqrt(2)/2.0)) * cell_size_px), PI / 4, Vector2(1, 1))
+	# draw a verticle rectangle in PINK
+	draw_rect(Rect2(
+		Vector2(cell_size_px / 2 - cell_size_px / 8, cell_size_px / 8),
+		Vector2(cell_size_px / 4, cell_size_px * 3 / 4)
+	),
+		colour
+	)
+	# draw a horizontal rectangle in PINK
+	draw_rect(Rect2(
+		Vector2(cell_size_px / 8, cell_size_px / 2 - cell_size_px / 8),
+		Vector2(cell_size_px * 3 / 4, cell_size_px / 4)
+	),
+		colour
+	)
 
 func check(data) -> bool:
 	return data[position_grid]["piece"] == null
